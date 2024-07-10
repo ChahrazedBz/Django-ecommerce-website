eb1a9f3433a309d0ad09dd715139128c438cd848
Bezzoudji Chahrazed Amina <chahrazed.bezzoudji.0404@univ-sba.dz>
2024-07-10 23:05:10 +0100

    Initial commit

---
+++ userauths/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100)
+    bio = models.CharField(max_length=100)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def _str_(self):
+        return self.username
---
+++ userauths/views.py
@@ -0,0 +1,59 @@
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+
+from userauths.forms import UserRegisterform
+
+User = settings.AUTH_USER_MODEL
+
+
+def register_view(request):
+
+    if request.method == "POST":
+        form = UserRegisterform(request.POST or None)
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(
+                request, f"Hey {username} ,Your account was created successufly"
+            )
+            new_user = authenticate(
+                username=form.cleaned_data["email"],
+                password=form.cleaned_data["password1"],
+            )
+            login(request, new_user)
+            return redirect("core:index")
+    else:
+        form = UserRegisterform()
+    context = {
+        "form": form,
+    }
+    return render(request, "userauths/sign-up.html", context)
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.warning(request, "Hey you are already logged in  ")
+
+        return redirect("core:index")
+
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.warning(request, f"user with {email} doesnt exist")
+
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "you are logged in")
+            return redirect("core:index")
+        else:
+            messages.warning(request, "User doesnt exist create a new account ")
+
+    context = {}
+    return render(request, "userauths/sign-in.html", context)
